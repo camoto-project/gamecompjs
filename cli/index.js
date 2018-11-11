5b1d77eacd0262990f3d52c3b2d47e6876bae97e
Adam Nielsen <malvineous@shikadi.net>
2018-11-11 11:35:53 +1000

    cli: Add ability to pass options to the algorithms

--- cli/index.js
+++ cli/index.js
@@ -8,9 +8,11 @@ const Debug = require('../util/utl-debug.js');
 const param = process.argv[2];
 
 if (['-h', '--help', '-?', '?', undefined].some(v => param == v)) {
-	console.log('gamecomp --list    // List algorithms');
-	console.log('gamecomp +example  // Compress/encrypt with "example" algorithm');
-	console.log('gamecomp -example  // Decompress/decrypt with "example"');
+	console.log('gamecomp --list | (+|-)format [opt=val [opt=val ...]]\n');
+	console.log('gamecomp --list       // List algorithms and their options');
+	console.log('gamecomp +example     // Compress/encrypt with "example" algorithm');
+	console.log('gamecomp -example     // Decompress/decrypt with "example"');
+	console.log('gamecomp +ex opt=123  // Pass parameter to algorithm');
 	process.exit(0);
 }
 
@@ -18,8 +20,18 @@ if (param == '--list') {
 	GameCompression.listHandlers().forEach(handler => {
 		const md = handler.metadata();
 		console.log(`${md.id}: ${md.title}`);
+		if (md.params) Object.keys(md.params).forEach(p => {
+			console.log(`  * ${p}: ${md.params[p]}`);
+		});
 	});
 	process.exit(0);
+}
+
+// Parse any name=value parameters
+let params = {};
+for (let i = 3; i < process.argv.length; i++) {
+	const [name, value] = process.argv[i].split('=');
+	params[name] = value;
 }
 
 let obscure;
@@ -46,9 +58,9 @@ if (!handler) {
 const content = fs.readFileSync(0, null);
 let outBuffer;
 if (obscure) {
-	outBuffer = handler.obscure(content);
+	outBuffer = handler.obscure(content, params);
 } else {
-	outBuffer = handler.reveal(content);
+	outBuffer = handler.reveal(content, params);
 }
 
 process.stdout.write(outBuffer);
