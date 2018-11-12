53cbde02ec5dedba26b522bb8160e020e7272bda
Adam Nielsen <malvineous@shikadi.net>
2018-11-13 00:07:17 +1000

    tests: Update source data for tests
    Put some repetitive data at the start to better test compression algorithms.

--- test/gen-cleartext.js
+++ test/gen-cleartext.js
@@ -1,11 +1,11 @@
 // This is what we expect the algorithm to produce when applied to the
 // default testdata.
-let standardCleartext = Buffer.alloc(256+16*2);
-Buffer.from('Example starting').copy(standardCleartext);
+let standardCleartext = Buffer.alloc(36+256+16);
+Buffer.from('she sells sea shells by the seashore').copy(standardCleartext);
 for (let i = 0; i < 256; i++) {
-	standardCleartext[i + 16] = i;
+	standardCleartext[i + 36] = i;
 }
-Buffer.from('Example finished').copy(standardCleartext, 256+16);
-//fs.writeFileSync('cleartxt.bin', standardCleartext);
+Buffer.from('Example finished').copy(standardCleartext, 256+36);
+//require('fs').writeFileSync('cleartxt.bin', standardCleartext);
 
 module.exports = standardCleartext;
